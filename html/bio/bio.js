2908350cb3eb84c8ccb7beef704880caa3d07d8d
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-09-01 22:37:24 -0700

    build with initial layout of home and bio

--- html/bio/bio.js
+++ html/bio/bio.js
@@ -4,15 +4,13 @@ let heading = require('../_templates/section-heading')(data);
 let body = `
 	<main>
 		${heading}
-		<section>
-			<img src="${data.img.src}" alt=${data.img.alt}>
-			<div class="text">
-				<p>${data.par_1}</p>
-				<p>${data.par_2}</p>
-				<p>${data.par_3}</p>
-				<p>${data.par_4}</p>
-			</div>
-		</section>
+		<img src="${data.img.src}" alt=${data.img.alt}>
+		<div class="text">
+			<p>${data.par_1}</p>
+			<p>${data.par_2}</p>
+			<p>${data.par_3}</p>
+			<p>${data.par_4}</p>
+		</div>
 	</main>
 `;
 
